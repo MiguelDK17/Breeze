3ee8ce0bd113937c396f635abe3d1c00d23c1515
MiguelDK17 <Miguelmf8633431@gmail.com>
2024-11-14 16:26:07 -0300

    Adicionado cores aos botões de Calendario, adicionado bordas e personalizações caso o botão for clicado

--- app/src/main/java/com/migueldk17/breeze/ui/components/Calendario.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/components/Calendario.kt
@@ -3,6 +3,8 @@ package com.migueldk17.breeze.ui.components
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +19,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,11 +27,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,6 +97,7 @@ fun Calendario(){
 @Composable
 fun GridMes(){
     val meses = listOf("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez")
+    var mesSelecionado by remember { mutableStateOf<String?>(null) }
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(3), // 3 colunas para os meses
@@ -99,34 +106,56 @@ fun GridMes(){
             .padding(8.dp) // padding para evitar que os meses fiquem colados nas bordas
     ) {
         items(meses.size) {index ->
-            MesItem(meses[index])
+            MesItem(
+                meses[index],
+                isSelected = meses[index] == mesSelecionado,
+                onClick = {mesSelecionado = meses[index]})
         }
     }
 
 }
 
 @Composable
-fun MesItem(mes: String){
+fun MesItem(
+    mes: String,
+    isSelected: Boolean,
+    onClick: () -> Unit){
+
+    val context = LocalContext.current
+
+
     Box(
         modifier = Modifier
             .padding(4.dp)
             .size(64.dp)
             .clip(RoundedCornerShape(12.dp)) //Tamanho de cada item
-            .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)) //Fundo com cor clara
+            .background(
+                if (isSelected) MaterialTheme.colorScheme.secondary.copy(alpha = 0.2f) // Caso for selecionado o card fica na cor mais forte
+                else MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)
+            )
+            .border(
+                width = if (isSelected) 2.dp else 0.dp, //Adiciona uma borda quando selecionado
+                color = if (isSelected) MaterialTheme.colorScheme.secondary else Color.Transparent,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .clickable {
+                onClick()
+                Toast.makeText(
+                    context, "Mês clicado: $mes", Toast.LENGTH_SHORT
+                ).show()
+            }
     ){
         Column(modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-           TextButton(onClick = {},
-               modifier = Modifier.fillMaxSize()) {
-               Text(
-                   text = mes,
-                   style = MaterialTheme.typography.bodyMedium, //Estilo de Texto
-                   color = MaterialTheme.colorScheme.onSurface
+            Text(
+                text = mes,
+                style = MaterialTheme.typography.bodyMedium, //Estilo de Texto
+                color = if (isSelected) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onSurface
                )
            }
-        }
+
     }
 }
 @Composable
