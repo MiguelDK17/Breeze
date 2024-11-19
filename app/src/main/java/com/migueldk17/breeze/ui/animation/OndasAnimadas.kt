39b192ceaea6d4b9307d2340dab3530b65ad545d
MiguelDK17 <Miguelmf8633431@gmail.com>
2024-11-19 18:08:04 -0300

    Melhorando animação de ondas em OndasAnimadas.kt

--- app/src/main/java/com/migueldk17/breeze/ui/animation/OndasAnimadas.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/animation/OndasAnimadas.kt
@@ -2,8 +2,10 @@ package com.migueldk17.breeze.ui.animation
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
@@ -13,50 +15,69 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import kotlin.math.cos
+import kotlin.math.hypot
+import kotlin.math.sin
 
 @Composable
 fun OndasAnimadas(
     baseColor: Color, //Cor base das ondas
-    onWaveEnd: () -> Unit
+    modifier: Modifier = Modifier
 ) {
-    Log.d(TAG, "OndasAnimadas: Início")
-    val waveRadius by animateFloatAsState(
-        targetValue = 2000f,
-        animationSpec = tween(1500, easing = FastOutSlowInEasing),
-        label = "animateFloatWave"
-    )
-    val alphaTransition by animateFloatAsState(
-        targetValue = 0.9f,
-        animationSpec = tween(1500, easing = LinearEasing),
-        label = "alphaTransition"
-    )
-    Log.d(TAG, "OndasAnimadas: $waveRadius")
-    //Chamado ao final da animação
-    LaunchedEffect(waveRadius) {
-        if (waveRadius >= 2000f) onWaveEnd()
+    val animatedRadius = remember {
+        Animatable(0f)
     }
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        drawCircle(
-            color = baseColor.copy(alphaTransition),
-            center = center,
-            radius = waveRadius
-        )
-    }
-    Log.d(TAG, "OndasAnimadas: Fim")
 
+    //Inicia a animação sempre que o targetColor mudar
+    LaunchedEffect(key1 = baseColor) {
+        animatedRadius.snapTo(0f) //Reinicia o raio
+        animatedRadius.animateTo(1f, animationSpec = tween(1000, easing = LinearOutSlowInEasing)) //Anima até o raio total
+    }
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        val canvasSize = size
+        val maxRadius = hypot(canvasSize.width, canvasSize.height) / 2
+        val canvasCenter = Offset(canvasSize.width / 2, canvasSize.height / 2)
+        val currentRadius = maxRadius * animatedRadius.value
+
+        val waveAmplitude = 30f
+        val waveFrequency = 10
+
+        val path = Path().apply {
+            val angleStep = 360f / waveFrequency
+            moveTo(canvasCenter.x + currentRadius, canvasCenter.y)
+
+            for (i in 0..waveFrequency){
+                val angle = Math.toRadians((angleStep * i).toDouble())
+                val offsetX = cos(angle).toFloat() * (currentRadius + waveAmplitude * sin(angle).toFloat())
+                val offsetY = sin(angle).toFloat() * (currentRadius + waveAmplitude * sin(angle).toFloat())
+                lineTo(canvasCenter.x + offsetX, canvasCenter.y + offsetY)
+            }
+            close()
+        }
+        drawPath(
+            path = path,
+            color = baseColor
+        )
+
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 private fun Preview(){
     Column(modifier = Modifier.fillMaxSize()){
-        OndasAnimadas(Color.Blue) { Log.d(TAG, "Preview: Fim da animação Preview") }
+        OndasAnimadas(MaterialTheme.colorScheme.primary)
     }
 }
--- app/src/main/java/com/migueldk17/breeze/ui/layouts/EditarValorConta.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/layouts/EditarValorConta.kt
@@ -2,10 +2,22 @@ package com.migueldk17.breeze.ui.layouts
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +31,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.guru.fontawesomecomposelib.FaIcon
+import com.guru.fontawesomecomposelib.FaIcons
 import com.migueldk17.breeze.ui.animation.OndasAnimadas
 
 @Composable
@@ -32,11 +46,25 @@ fun EditarValorConta(/*navController: NavController*/){
         horizontalAlignment = Alignment.CenterHorizontally,
 
     ) {
-        TextField(text, onValueChange = { value ->
-            text = value
-        },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(text, onValueChange = { value ->
+                text = value
+            },
+                placeholder = {
+                    Text("Valor")
+                },
+                modifier = Modifier.size(width = 110.dp, height = 70.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(Modifier.width(10.dp))
+            OutlinedIconButton(onClick = {}) {
+                Icon(Icons.Outlined.Check, contentDescription = null)
+            }
+        }
         Spacer(modifier = Modifier.height(150.dp))
     }
 
