4df502be117c6e83d4c907e86751fbea8239b846
MiguelDK17 <Miguelmf8633431@gmail.com>
2025-01-09 12:32:57 -0300

    Inciando desenvolvimento da função para adicionar contas secundárias

--- app/src/main/java/com/migueldk17/breeze/ui/components/BreezeBottomBar.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/components/BreezeBottomBar.kt
@@ -1,8 +1,13 @@
 package com.migueldk17.breeze.ui.components
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -14,8 +19,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
@@ -24,62 +33,70 @@ import com.migueldk17.breeze.Screen
 @Composable
 fun BreezeBottomBar(navController: NavController){
     var selectedItem by remember { mutableIntStateOf(0) }
-    NavigationBar{
-        NavigationBarItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Página Inicial") },
-            label = { Text("Página Inicial") },
-            selected = selectedItem == 0,
-            onClick = {
-                selectedItem = 0
-                navController.navigate(Screen.PaginaInicial.route)
-            },
-            colors = NavigationBarItemColors(
-                selectedIconColor = MaterialTheme.colorScheme.secondary,
-                selectedTextColor = MaterialTheme.colorScheme.secondary,
-                selectedIndicatorColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.secondary,
-                unselectedTextColor = MaterialTheme.colorScheme.secondary,
-                disabledIconColor = Color.Unspecified,
-                disabledTextColor = Color.Unspecified,
+
+    NavigationBar {
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Home, contentDescription = "Página Inicial") },
+                label = { Text("Página Inicial") },
+                selected = selectedItem == 0,
+                onClick = {
+                    selectedItem = 0
+                    navController.navigate(Screen.PaginaInicial.route)
+                },
+                colors = NavigationBarItemColors(
+                    selectedIconColor = MaterialTheme.colorScheme.secondary,
+                    selectedTextColor = MaterialTheme.colorScheme.secondary,
+                    selectedIndicatorColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.secondary,
+                    unselectedTextColor = MaterialTheme.colorScheme.secondary,
+                    disabledIconColor = Color.Unspecified,
+                    disabledTextColor = Color.Unspecified,
+                )
             )
-        )
-        NavigationBarItem(
-            icon = { FaIcon(FaIcons.CalendarRegular, tint = MaterialTheme.colorScheme.secondary) },
-            label = { Text("Histórico") },
-            selected = selectedItem == 1,
-            onClick = {
-                selectedItem = 1
-                navController.navigate(Screen.Historico.route)
-            },
-            colors = NavigationBarItemColors(
-                selectedIconColor = Color.Unspecified, //Cor do icone aqui não afeta icones da biblioteca FAIcon
-                selectedTextColor = MaterialTheme.colorScheme.secondary,
-                selectedIndicatorColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = Color.Unspecified,
-                unselectedTextColor = MaterialTheme.colorScheme.secondary,
-                disabledIconColor = Color.Unspecified,
-                disabledTextColor = Color.Unspecified,
+            NavigationBarItem(
+                icon = {
+                    FaIcon(
+                        FaIcons.CalendarRegular,
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                label = { Text("Histórico") },
+                selected = selectedItem == 1,
+                onClick = {
+                    selectedItem = 1
+                    navController.navigate(Screen.Historico.route)
+                },
+                colors = NavigationBarItemColors(
+                    selectedIconColor = Color.Unspecified, //Cor do icone aqui não afeta icones da biblioteca FAIcon
+                    selectedTextColor = MaterialTheme.colorScheme.secondary,
+                    selectedIndicatorColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = Color.Unspecified,
+                    unselectedTextColor = MaterialTheme.colorScheme.secondary,
+                    disabledIconColor = Color.Unspecified,
+                    disabledTextColor = Color.Unspecified,
+                )
             )
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Filled.Settings, contentDescription = "") },
-            label = { Text("Configurações") },
-            selected = selectedItem == 2,
-            onClick = {
-                selectedItem = 2
-                navController.navigate(Screen.Configuracoes.route)
-            },
-            colors = NavigationBarItemColors(
-                selectedIconColor = MaterialTheme.colorScheme.secondary,
-                selectedTextColor = MaterialTheme.colorScheme.secondary,
-                selectedIndicatorColor = MaterialTheme.colorScheme.primary,
-                unselectedIconColor = MaterialTheme.colorScheme.secondary,
-                unselectedTextColor = MaterialTheme.colorScheme.secondary,
-                disabledIconColor = Color.Unspecified,
-                disabledTextColor = Color.Unspecified,
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Settings, contentDescription = "") },
+                label = { Text("Configurações") },
+                selected = selectedItem == 2,
+                onClick = {
+                    selectedItem = 2
+                    navController.navigate(Screen.Configuracoes.route)
+                },
+                colors = NavigationBarItemColors(
+                    selectedIconColor = MaterialTheme.colorScheme.secondary,
+                    selectedTextColor = MaterialTheme.colorScheme.secondary,
+                    selectedIndicatorColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.secondary,
+                    unselectedTextColor = MaterialTheme.colorScheme.secondary,
+                    disabledIconColor = Color.Unspecified,
+                    disabledTextColor = Color.Unspecified,
+                )
             )
-        )
-    }
+        }
+
+
 }
 
 @Composable
--- app/src/main/java/com/migueldk17/breeze/ui/components/BreezeTopAppBar.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/components/BreezeTopAppBar.kt
@@ -2,28 +2,21 @@ package com.migueldk17.breeze.ui.components
 
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.migueldk17.breeze.ui.theme.Branco
 import com.migueldk17.breeze.ui.theme.BreezeTheme
 
 
@@ -44,12 +37,10 @@ fun BreezeTopAppBar(){
                         context, "Botão para adicionar contas", Toast.LENGTH_SHORT
                     ).show()
                 },
-                modifier = Modifier.background(Branco)
             ) {
-                Icon(Icons.Outlined.AddCircle,
+                Icon(Icons.Default.AddCircle,
                     "Adicionar contas secundárias",
-                    modifier = Modifier.size(28.dp)
-                        .background(Branco))
+                    modifier = Modifier.size(28.dp))
             }
         })
 }
