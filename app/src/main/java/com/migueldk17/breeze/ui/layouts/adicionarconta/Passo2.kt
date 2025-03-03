205caa81a755aebe3b8fe9b3c71cb7cd87b7d438
MiguelDK17 <Miguelmf8633431@gmail.com>
2025-03-03 17:23:37 -0300

    Add a Spacer to align content in Passo2.kt and modified Row fillMaxSize.

--- app/src/main/java/com/migueldk17/breeze/ui/layouts/adicionarconta/Passo2.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/layouts/adicionarconta/Passo2.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -67,9 +68,11 @@ fun Passo2(navController: NavController, viewModel: BreezeViewModel = hiltViewMo
 
             ) {
                 Row(
-                    modifier = Modifier.padding(10.dp)
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxSize()
                 ) {
-                    Spacer(Modifier.size(50.dp))
+                    Spacer(Modifier.size(70.dp))
                     Column {
                         Text(nomeConta,
                             style = MaterialTheme.typography.bodySmall.copy(
