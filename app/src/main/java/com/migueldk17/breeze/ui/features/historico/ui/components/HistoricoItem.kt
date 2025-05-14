6dda84c0b87484f1c3e734cb691158eb31c1fb3a
MiguelDK17 <Miguelmf8633431@gmail.com>
2025-05-14 17:01:44 -0300

    Refactor: Improve Historico components and remove unnecessary imports
    - Removed unused imports in `BoxDate.kt`, `HistoricoItem.kt`, `ContaPrincipal.kt`, and `ContaSecundaria.kt` to clean up the code.
    - Updated the `HistoricoItem` component to improve its visual presentation when displaying multiple accounts.

--- app/src/main/java/com/migueldk17/breeze/ui/features/historico/ui/components/HistoricoItem.kt
+++ app/src/main/java/com/migueldk17/breeze/ui/features/historico/ui/components/HistoricoItem.kt
@@ -1,55 +1,29 @@
 package com.migueldk17.breeze.ui.features.historico.ui.components
 
-import android.util.Log
+
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.migueldk17.breezeicons.icons.BreezeIcon
-import com.github.migueldk17.breezeicons.icons.BreezeIcons
 import com.github.migueldk17.breezeicons.icons.BreezeIconsType
-import com.migueldk17.breeze.converters.toBreezeIconsType
-import com.migueldk17.breeze.converters.toLocalDateTime
 import com.migueldk17.breeze.entity.Conta
-import com.migueldk17.breeze.ui.theme.PastelLightBlue
-import com.migueldk17.breeze.ui.utils.formataSaldo
 import java.time.LocalDate
-import android.content.ContentValues.TAG
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalDensity
-
-import com.migueldk17.breeze.ui.theme.Blue
 
 @Composable
 fun HistoricoItem(
